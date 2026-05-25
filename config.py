1ac760263bb283e7507a7f2f1b6ec46f39092304
Meribe Henry <meribehenry09@gmail.com>
2026-05-25 15:43:50 +0100

    Added FLASK_APP attribute

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    FLASK_APP=os.environ.get("FLASK_APP")
     SECRET_KEY=os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URL", "sqlite:///app.db")
     MAX_CONTENT_LENGTH=5242880

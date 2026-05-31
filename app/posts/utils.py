803dfc99029282eca82b23696e397b96a9f8fb1d
Meribe Henry <meribehenry09@gmail.com>
2026-05-31 22:00:00 +0100

    Added count_clicks function that prevent user that have clicked on a post, clicks from counting again. It stores each clicks on the database (clicks table)

--- app/posts/utils.py
+++ app/posts/utils.py
@@ -1,3 +1,5 @@
+from app.models import Clicks
+from app.extensions import db
 import secrets
 import cloudinary.uploader
 
@@ -16,3 +18,13 @@ def save_photo(photo):
 
 def delete_photo(public_id):
     cloudinary.uploader.destroy(public_id, invalidate=True)
+
+def count_clicks(post, current_user):
+    click = Clicks.query.filter_by(post_id=post.id, user_id=current_user.id).first()
+    if not click:
+        if post.author != current_user:
+            click = Clicks(post_id=post.id, viewed=True, user_id=current_user.id, username=current_user.username)
+            click.viewed = True
+            post.num_of_clicks = post.num_of_clicks + 1
+            db.session.add(click)
+            db.session.commit()
